f2662f4a8793f0025dbafcaf0b5aaf9ddeef3f4f
danitex24 <fanbase.ng@gmail.com>
2025-06-05 21:55:54 +0100

    Fired when the plugin is uninstalled

--- uninstall.php
+++ uninstall.php
@@ -1,1 +1,21 @@
-// Placeholder for uninstall.php
+<?php
+/**
+ * Fired when the plugin is uninstalled.
+ *
+ * @package    Kura_AI
+ */
+
+// If uninstall not called from WordPress, then exit.
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    exit;
+}
+
+// Delete plugin options
+delete_option('kura_ai_settings');
+
+// Delete database tables
+global $wpdb;
+$wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}kura_ai_logs");
+
+// Clear scheduled events
+wp_clear_scheduled_hook('kura_ai_daily_scan');
