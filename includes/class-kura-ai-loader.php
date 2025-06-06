29b5ec87965acea55e3bfce6f716531561b2e032
danitex24 <fanbase.ng@gmail.com>
2025-06-06 01:02:13 +0100

    change priority to 10

--- includes/class-kura-ai-loader.php
+++ includes/class-kura-ai-loader.php
@@ -52,7 +52,7 @@ class Kura_AI_Loader
      * @param    int       $priority         Optional. The priority at which the function should be fired. Default is 10.
      * @param    int       $accepted_args    Optional. The number of arguments that should be passed to the $callback. Default is 1.
      */
-    public function add_action($hook, $component, $callback, $priority = 3, $accepted_args = 1)
+    public function add_action($hook, $component, $callback, $priority = 10, $accepted_args = 1)
     {
         $this->actions = $this->add($this->actions, $hook, $component, $callback, $priority, $accepted_args);
     }
@@ -67,7 +67,7 @@ class Kura_AI_Loader
      * @param    int       $priority         Optional. The priority at which the function should be fired. Default is 10.
      * @param    int       $accepted_args    Optional. The number of arguments that should be passed to the $callback. Default is 1.
      */
-    public function add_filter($hook, $component, $callback, $priority = 3, $accepted_args = 1)
+    public function add_filter($hook, $component, $callback, $priority = 10, $accepted_args = 1)
     {
         $this->filters = $this->add($this->filters, $hook, $component, $callback, $priority, $accepted_args);
     }
